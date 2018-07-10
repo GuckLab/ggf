98b5ae6b53aab75f71d8e1c595265996f04483e1
Paul Müller <dev@craban.de>
2018-07-10 10:03:42 +0200

    update scan parameters

--- sge/scan.py
+++ sge/scan.py
@@ -3,32 +3,34 @@ import sys
 from manager import PM_Client, PM_Server
 
 
-if __name__ == "__main__":
-    # default parameters
-    kwargs = dict(model="boyde2009",
-                  semi_major=10e-6,
-                  semi_minor=10e-6,
-                  object_index=1.3776,
-                  medium_index=1.335,
-                  effective_fiber_distance=180e-6,
-                  mode_field_diameter=2.4e-6,
-                  power_per_fiber=.65,
-                  wavelength=780e-9,
-                  poisson_ratio=0.5,
-                  n_poly=None,
-                  verbose=False)
+# default parameters
+kwargs = dict(model="boyde2009",
+              semi_major=10e-6,
+              semi_minor=10e-6,
+              object_index=1.3776,
+              medium_index=1.335,
+              effective_fiber_distance=180e-6,
+              mode_field_diameter=4.8e-6,
+              power_per_fiber=.65,
+              wavelength=780e-9,
+              poisson_ratio=0.5,
+              n_poly=None,
+              verbose=False)
 
-    # changed parameters (each item will produce a new scan)
-    server_args = [[["semi_major", 5e-6, 20e-6, 10]],
-                   [["object_index", 1.34, 1.39, 10]],
-                   [["medium_index", 1.33, 1.34, 10]],
-                   [["effective_fiber_distance", 150e-6, 200e-6, 10]],
-                   [["mode_field_diameter", 2e-6, 3e-6, 10]],
-                   [["power_per_fiber", .5, .8, 10]],
-                   [["wavelength", 700e-9, 820e-9, 10]],
-                   [["poisson_ratio", 0.4, .5, 10]],
-                   [["n_poly", 20, 200, 10]],
-                   ]
+# changed parameters (each item will produce a new scan)
+server_args = [[["semi_major", 10e-6, 15e-6, 10]],
+               [["object_index", 1.34, 1.39, 10]],
+               [["medium_index", 1.33, 1.34, 10]],
+               [["effective_fiber_distance", 150e-6, 200e-6, 10]],
+               [["mode_field_diameter", 3e-6, 6e-6, 10]],
+               [["power_per_fiber", .5, .8, 10]],
+               [["wavelength", 700e-9, 820e-9, 10]],
+               [["poisson_ratio", 0.4, .5, 10]],
+               [["n_poly", 20, 200, 10]],
+               ]
+
+if __name__ == "__main__":
+
     
     mode = sys.argv[-1]
     
