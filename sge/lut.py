e2d719b6e791465d73ca3833f4e50e2d765298e2
Paul Müller <dev@craban.de>
2018-07-16 13:09:46 +0200

    remove open cell lut

--- sge/lut.py
+++ sge/lut.py
@@ -4,19 +4,6 @@ from lut_manager import PM_Client, PM_Server
 
 
 luts = {
-    "guck_open_cell":
-        {"model": "boyde2009",
-         "stretch_ratio": [0, .15, 40],
-         "semi_minor": [6.5e-6, 8e-6, 15],
-         "relative_object_index": [1.02, 1.03, 5],
-         "medium_index": [1.333, 1.338, 2],
-         "effective_fiber_distance": [170e-6, 190e-6, 2],
-         "mode_field_diameter": 4.8e-6,
-         "power_per_fiber": .65,
-         "wavelength": 780e-9,
-         "poisson_ratio": [0.4, .5, 2],
-         "n_poly": 120,
-         },
     "guck_open_fus":
         {"model": "boyde2009",
          "stretch_ratio": [0, .04, 12],
@@ -43,6 +30,19 @@ luts = {
          "poisson_ratio": [0.4, .5, 2],
          "n_poly": 120,
          },
+#    "guck_open_cell":
+#        {"model": "boyde2009",
+#         "stretch_ratio": [0, .15, 40],
+#         "semi_minor": [6.5e-6, 8e-6, 15],
+#         "relative_object_index": [1.02, 1.03, 5],
+#         "medium_index": [1.333, 1.338, 2],
+#         "effective_fiber_distance": [170e-6, 190e-6, 2],
+#         "mode_field_diameter": 4.8e-6,
+#         "power_per_fiber": .65,
+#         "wavelength": 780e-9,
+#         "poisson_ratio": [0.4, .5, 2],
+#         "n_poly": 120,
+#         },
     }
 
 
